f2fe97fb140ffdef09680ae5b7cb2b5e3539c182
Brian-MT-Nguyen <greencheezin@gmail.com>
2023-06-06 02:01:03 -0700

    changed workflow of prototypes to be standalone

--- prototypes/coregamplay.js
+++ prototypes/coregamplay.js
@@ -48,10 +48,6 @@ class CoreGameplay extends SchismScene {
         // Player Physics
         this.physics.add.collider(this.player, this.floor);
         this.physics.add.collider(this.player, this.worldbounds);
-
-        this.time.delayedCall(1000, () => {
-            this.timeTravel('sceneflow');
-        });
     }
 
     update() {
@@ -59,3 +55,25 @@ class CoreGameplay extends SchismScene {
         this.player.update();
     }
 }
+
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x : 0,
+                y : 500
+            }
+        }
+    },
+    backgroundColor: 0x000000,
+    scene: [CoreGameplay],
+    title: "Schism"
+});
--- prototypes/sceneflow.js
+++ prototypes/sceneflow.js
@@ -49,7 +49,9 @@ class SceneFlow extends SchismScene {
         this.physics.add.collider(this.player, this.floor);
         this.physics.add.collider(this.player, this.worldbounds);
 
-        //this.gotoScene('sceneflow');
+        this.time.delayedCall(1000, () => {
+            this.timeTravel('coregameplay');
+        });
     }
 
     update() {
@@ -65,3 +67,83 @@ class SceneFlow extends SchismScene {
     }
 
 }
+
+class CoreGameplay extends SchismScene {
+    constructor() {
+        super("coregameplay", "Deez Nuts");
+    }
+
+    preload() {
+        //characters
+        this.load.path = '../assets/character/';
+        this.load.image('lunebase', 'luneBaseSprite.png');
+        this.load.image('solBase', 'solBaseSprite.png');
+        this.load.image('solSit', 'solSitting.png');
+
+
+        //levels
+        this.load.path = '../assets/levels/';
+        this.load.image('office', 'officeLvlPast.png');
+    }
+    
+    onEnter() {
+        // Create background
+        let bg = this.add.image(0, 0, 'office').setOrigin(0);
+
+        // Create Player
+        this.player = new Player(this, 300, 1035, 'lunebase');
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0, 0, bg.width, bg.height);
+
+        // Create floor
+        this.floor = this.add.rectangle(0, 1250, 2560, 100).setOrigin(0);
+        this.physics.add.existing(this.floor);
+        this.floor.body.allowGravity = false;
+        this.floor.body.immovable = true;
+
+        // Create world bounds
+        this.worldbounds = this.add.group();
+        this.lWall = this.add.rectangle(-100,0,100,1920).setOrigin(0);
+        this.physics.add.existing(this.lWall);
+        this.lWall.body.allowGravity = false;
+        this.lWall.body.immovable = true;
+        this.worldbounds.add(this.lWall);
+
+        this.rWall = this.add.rectangle(2560,0,100,1920).setOrigin(0);
+        this.physics.add.existing(this.rWall);
+        this.rWall.body.allowGravity = false;
+        this.rWall.body.immovable = true;
+        this.worldbounds.add(this.rWall);
+
+        // Player Physics
+        this.physics.add.collider(this.player, this.floor);
+        this.physics.add.collider(this.player, this.worldbounds);
+    }
+
+    update() {
+        // Update Player Logics
+        this.player.update();
+    }
+}
+
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x : 0,
+                y : 500
+            }
+        }
+    },
+    backgroundColor: 0x000000,
+    scene: [SceneFlow, CoreGameplay],
+    title: "Schism"
+});
