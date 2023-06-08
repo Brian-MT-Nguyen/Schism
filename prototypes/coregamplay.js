2d091cb634b34f7878ac2c5ff724d341be54a348
ajhalim <thenubbum@gmail.com>
2023-06-08 11:05:39 -0700

    Update coregamplay.js

--- prototypes/coregamplay.js
+++ prototypes/coregamplay.js
@@ -1,4 +1,9 @@
 class CoreGameplay extends SchismScene {
+
+    //acursors;
+
+    goal;
+
     constructor() {
         super("coregameplay", "Deez Nuts");
     }
@@ -17,6 +22,10 @@ class CoreGameplay extends SchismScene {
     }
     
     onEnter() {
+
+       //let rKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+       
         // Create background
         let bg = this.add.image(0, 0, 'office').setOrigin(0);
 
@@ -24,6 +33,13 @@ class CoreGameplay extends SchismScene {
         this.player = new Player(this, 300, 1035, 'lunebase');
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0, 0, bg.width, bg.height);
+
+        //goal
+        //this.goal = this.Phaser.Physics.Arcade.Sprite.add.staticGroup();
+
+        //this.goal.create(2560*.95, 1920*.59,'solSit').refreshBody()
+
+        this.goal = this.physics.add.sprite(2560*.5, 1920*.5,'lunebase')
 
         // Create floor
         this.floor = this.add.rectangle(0, 1250, 2560, 100).setOrigin(0);
@@ -48,6 +64,63 @@ class CoreGameplay extends SchismScene {
         // Player Physics
         this.physics.add.collider(this.player, this.floor);
         this.physics.add.collider(this.player, this.worldbounds);
+
+
+        this.physics.add.collider(this.goal, this.floor);
+        this.physics.add.collider(this.goal, this.worldbounds);
+
+        //this.physics.add.collider(this.player, this.goal);
+
+        
+
+        this.physics.add.overlap(this.player, this.goal, console.log("dog"));
+        //this.physics.add.overlap(this.player, this.goal, null, null, this);
+    }
+
+    switchLevel = (oldScene, newSceneString) => {
+        console.log(`From ${oldScene.scene.key} to ${newSceneString}`);
+        oldScene.scene.start(newSceneString);
+      };
+
+    update() {
+        // Update Player Logics
+        this.player.update();
+
+        //this.physics.add.overlap(this.player, this.goal, console.log("dog"), null, this);
+
+    }
+}
+
+class CoreGameplay2 extends SchismScene {
+    constructor() {
+        super("CoreGameplay2", "testing");
+    }
+
+    preload() {
+        this.load.path = '../assets/character/';
+        this.load.image('lunebase', 'luneBaseSprite.png');
+        this.load.image('solBase', 'solBaseSprite.png');
+        this.load.image('solSit', 'solSitting.png');
+
+        //levels
+        this.load.path = '../assets/levels/';
+        this.load.image('office', 'officeLvlPast.png');
+    }
+
+    onEnter() {
+        // Create background
+        let bg = this.add.image(0, 0, 'office').setOrigin(0);
+
+        // Create Player
+        this.player = new Player(this, 300, 1035, 'lunebase');
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0, 0, bg.width, bg.height);
+
+        // Create floor
+        this.floor = this.add.rectangle(0, 1250, 2560, 100).setOrigin(0);
+        this.physics.add.existing(this.floor);
+        this.floor.body.allowGravity = false;
+        this.floor.body.immovable = true;
     }
 
     update() {
@@ -74,6 +147,6 @@ const game = new Phaser.Game({
         }
     },
     backgroundColor: 0x000000,
-    scene: [CoreGameplay],
+    scene: [CoreGameplay, CoreGameplay2],
     title: "Schism"
 });
