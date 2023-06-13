ee6fd348725047458dcef2a50f279dff13434bdd
ajhalim <thenubbum@gmail.com>
2023-06-13 12:12:12 -0700

    moved to main

--- prefabs/UI.js
+++ prefabs/UI.js
@@ -5,15 +5,23 @@ class UI extends Phaser.Physics.Arcade.Sprite {
         super(left, right, up, time, interact, mute, fullscreen)
     } */
 
-    constructor(scene, left){
-        super(scene, left);
+    constructor(scene, arrow, follow){
+        super(scene, arrow, follow);
 
         //left
-        this.lPress = scene.add.sprite(2560 * .2,1920 * .7,left).setScale(0.12);
+        this.lPress = scene.add.sprite(2560 * .2,1920 * .7,arrow).setScale(0.12);
+        this.lPress.setScrollFactor(0)
+        //this.lPress.main.startFollow(follow);
 
         //right
-        this.rPress = scene.add.sprite(2560 * .10,1920 * .7,left).setScale(0.12)
+        this.rPress = scene.add.sprite(2560 * .05,1920 * .7,arrow).setScale(0.12)
         this.rPress.flipX=true;
+
+        //up
+        this.uPress = scene.add.sprite(2560 * .125,1920 * .7,arrow).setScale(0.12)
+        this.uPress.angle += 270;
+
+        
         //dscene.physics.world.enable(this);
         // /this.setOrigin(0.1).setScale(0.8);
     }
