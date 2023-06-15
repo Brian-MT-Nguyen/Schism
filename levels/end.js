92a32aa7c98370b637138c61be445056ff9e71ee
ajhalim <thenubbum@gmail.com>
2023-06-15 16:23:11 -0700

    end

--- levels/end.js
+++ levels/end.js
@@ -13,9 +13,18 @@ class ending extends Phaser.Scene {
 
         let bg = this.add.image(0, 0, 'end').setOrigin(0).setDepth(envDepth);
 
-        this.add.text(1920 *.3, 1080 *.7,"Day 987: Lune, if you find this message, then you will grow to be disappointed upon my absence.").setDepth(playerDepth);
-        this.add.text(1920 *.3, 1080 *.72,"Ever since you came into this world, I knew you would grow the heart to look for your creator.").setDepth(playerDepth);
-        this.add.text(1920 *.3, 1080 *.74,"I established that in you. However, after I have learned that the state of this world has fallen. I had to create something to take care of Sol.").setDepth(playerDepth);
-        this.add.text(1920 *.3, 1080 *.72,"Ever since you came into this world\n I knew you would grow the heart to look for your creator.").setDepth(playerDepth);
+        this.time.delayedCall(1000, () => {this.t1 = this.add.text(1920 *.1, 1080 *.7,"Day 987: Lune, if you find this message, then you will grow to be disappointed upon my absence.").setDepth(playerDepth);} ,this)
+        
+        this.time.delayedCall(2000, () => {this.t2 = this.add.text(1920 *.1, 1080 *.73,"Ever since you came into this world, I knew you would grow the heart to look for your creator.").setDepth(playerDepth);}, this);
+
+        this.time.delayedCall(3000, () => {this.t3 = this.add.text(1920 *.1, 1080 *.76,"I established that in you. However, after I have learned that the state of this world has fallen.").setDepth(playerDepth).set;}, this);
+
+        this.time.delayedCall(4000, () => {this.t4 = this.add.text(1920 *.3, 1080 *.79,"I had to create something to take care of Sol.").setDepth(playerDepth);}, this);
+
+        this.t1.setFontSize(28);
+        this.t2.setFontSize(28);
+        this.t3.setFontSize(28);
+        this.t4.setFontSize(28);
     }
+    
 }
