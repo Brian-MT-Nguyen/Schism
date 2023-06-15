93e055041ffbec0434fd6570f082d60f8dd548d0
Brian-MT-Nguyen <greencheezin@gmail.com>
2023-06-15 14:57:26 -0700

    added image to end screen

--- levels/end.js
+++ levels/end.js
@@ -9,21 +9,10 @@ class ending extends Phaser.Scene {
         this.load.image('end', 'endscreen.png');
     }
 
-    onEnter() {
-        //let circle = this.add.image(1920 *.51, 1080 *.5, 'player').setScale(1.5)
-        //this.add.thing(1920 *.5, 1080 *.5,"player")
-        /* this.add.text(2560 *.5, 1920 *.5,"you did it");
-        this.add.text(2560 *.47, 1920 *.6,":>"); */
+    create() {
 
         let bg = this.add.image(0, 0, 'end').setOrigin(0).setDepth(envDepth);
 
-
-        //this.input.on('pointerdown', () => this.scene.start('lvl3'));
-    }
-
-    update(){
-        //console.log("dog");
-        this.add.text(1920 *.5, 1080 *.5,"you did it");
-        //this.add.text(2560 *.5, 1920 *.6,":>");
+        this.add.text(1920 *.5, 1080 *.5,"you did it").setDepth(playerDepth);
     }
 }
